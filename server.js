291f4b19aa19abf1f32694e2c389439fb80c9ef8
Will Wenhua Yang <will.whyang@gmail.com>
2017-11-25 23:08:44 +1100

    remove demo code in server.js

--- server.js
+++ server.js
@@ -25,16 +25,6 @@ app.use(session({
 app.use(morgan('combined'))
 app.use(express.static(`${__dirname}/public`))
 
-const {MyComponent} = require('./blah.jsx')
-
-app.get('/reactdemo', (req, res) => {
-  const elem = react.createElement(MyComponent, {name:'fred', items:['a', 'b']})
-  const stream = ReactDOMServer.renderToStaticNodeStream(elem)
-
-  res.set('content-type', 'text/html')
-  stream.pipe(res)
-})
-
 app.get('/', (req, res) => {
   res.send('oh hai')
 })
