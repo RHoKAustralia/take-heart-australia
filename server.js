554636311bcb6edbf1b9100ac72f32d3bb43a3df
Joseph Gentle <me@josephg.com>
2017-11-25 15:15:35 +1100

    Added example jsx static render

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const react = require('react')
+const ReactDOMServer = require('react-dom/server')
+
 const express = require('express')
+require('node-jsx').install()
+
 const app = express()
 
 app.use(express.static(`${__dirname}/public`))
+
+const {MyComponent} = require('./blah.jsx')
+
+app.get('/reactdemo', (req, res) => {
+  const elem = react.createElement(MyComponent, {name:'fred', items:['a', 'b']})
+  const stream = ReactDOMServer.renderToStaticNodeStream(elem)
+
+  res.set('content-type', 'text/html')
+  stream.pipe(res)
+})
 
 app.get('/', (req, res) => {
   res.send('oh hai')
