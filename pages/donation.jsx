26c9f1c40fec979459aa98fe9766e68e527b7d9a
Will Wenhua Yang <will.whyang@gmail.com>
2017-11-25 15:19:08 +1100

    starter code for donation

---
+++ pages/donation.jsx
@@ -0,0 +1,3 @@
+var stripe = require("stripe")(
+    "sk_test_sPgf3XjouxQvdAuFWsPH5lVd"
+);
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
+var session = require('express-session')
+
 const app = express()
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
 
 app.use(express.static(`${__dirname}/public`))
 
 app.get('/', (req, res) => {
   res.send('oh hai')
+})
+
+app.get('/donation', (req, res) => {
+  req.session.test1 = {a: 10, b: 'bbb'}
+  console.log(1, req.session.test1)
+  res.send('abc')
 })
 
 
