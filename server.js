6635f1911b0f3149f74b6a5f2c2097a7d909eee8
Joseph Gentle <me@josephg.com>
2017-11-25 16:10:24 +1100

    Added postgres starter

--- server.js
+++ server.js
@@ -22,6 +22,35 @@ app.get('/', (req, res) => {
   res.send('oh hai')
 })
 
+// createdb tha
+const sql = require('sql-template-strings')
+const pg = require('pg')
+const pool = new pg.Pool({
+  database: 'tha'
+})
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+})
+
+const withDb = (req, res, next) => {
+  pool.connect((err, client, done) => {
+    if (err) return next(err)
+    req.db = client
+    const finished = () => { if (done) { done(); done = null } }
+    res.on('finish', finished)
+    res.on('close', finished)
+    next()
+  })
+}
+
+app.get('/dbtest', withDb, (req, res, next) => {
+  req.db.query(sql`SELECT NOW()`, (err, result) => {
+    if (err) return next(err)
+    //console.log(err, result.rows[0])
+    res.send(result.rows[0])
+  })
+})
 
 require('http').createServer(app).listen(3939)
 console.log('server listening on http://localhost:3939')
